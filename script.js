8efa718c5a88a24efd6393d0225602f3f3732aa7
RDF014 <rdf014@gmail.com>
2017-01-24 03:29:13 -0800

    Completed bare minimum?

--- script.js
+++ script.js
@@ -14,25 +14,45 @@ $(document).ready(function(){
   // var $time = $('<b></b>');
   // $time.text(time);
 
-
-
   var index = streams.home.length - 1;
   while(index >= 0){
-    //access nested object that holds user, tweet, and time
-    var $section = $('<section></section>')
+    var $section = $('<section class=\"userName\"></section>');
     var tweet = streams.home[index];
-    var $user = $('<h3><a href=\"#\">@'+tweet.user+'</a></h3>');
+    var $user = $('<h3><a href=\"#\">@' + tweet.user + '</a></h3>');
     var $timeCreate = $('<h6>'+ tweet.created_at +'</h6>');
     var $tweet = $('<p>'+ tweet.message +'</p>');
+    var $pastList = $('<ul></ul>').hide();
     // $tweet.text(tweet.message);
 
     $section.appendTo($body);
     $user.appendTo($section);
     $timeCreate.appendTo($section)
     $tweet.appendTo($section);
+    $pastList.appendTo($section);
+
+
+    var userIndex = streams.users[tweet.user];
+    for(var i = userIndex.length-2; i >= 0; i--){
+      //iterate through the user array: pulling the message and time
+        //do as above except with li element
+      var userPast = userIndex[i];
+      var $pastTime = $('<h6>'+ userPast.created_at +'</h6>');
+      var $pastTweet = $('<p>'+ userPast.message +'</p>');
+      var $unorder = $('<li></li>');
+
+      $unorder.appendTo($pastList);
+      $pastTime.appendTo($unorder);
+      $pastTweet.appendTo($unorder);
+    }
+
 
     index -= 1;
   }
+
+  $('.userName').on('click', function(){
+    event.preventDefault();
+    $(this).find('ul').fadeToggle();
+  })
 
 
 });
